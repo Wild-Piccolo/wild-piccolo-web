e2a387b60f783feabc895fdc21c328fb2ce570da
Suyash Aditya Rajesh <suyashrajesh@gmail.com>
2025-10-12 19:07:52 -0400

    Navigation: Added routing setup and Home/Products components.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; 
+import Home from "./Home/Home";
+import Products from "./Products/Products";
+
 
 function App() {
-  // Define the sidebar open/close functions here
   function openMenu() {
     document.querySelector(".sidebar")?.classList.add("open");
   }
@@ -12,121 +15,47 @@ function App() {
   }
 
   return (
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button onClick={openMenu}>&#9776;</button>
-          <a href="index.html">Wild Piccolo</a>
-        </div>
-        <div className="header-links">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-      </header>
+    <Router>
+      <div className="grid-container">
+        
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">Wild Piccolo</Link> 
+          </div>
+          
+          <div className="header-links">
+            <Link to="/">Home</Link>
+            <Link to="/catalog">Catalog</Link>
+          </div>
+          
+        </header>
 
-      <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>
-          x
-        </button>
-        <ul>
-          <li>
-            <a href="index.html">Pants</a>
-          </li>
-          <li>
-            <a href="index.html">Shirts</a>
-          </li>
-        </ul>
-      </aside>
-
-      <main className="main">
-        <div className="content">
-          <ul className="products">
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
+          <ul>
             <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
+              <Link to="/products/pants">Pants</Link>
             </li>
-
             <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-
-            <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-
-            <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-
-            <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
+              <Link to="/products/shirts">Shirts</Link>
             </li>
           </ul>
-        </div>
-      </main>
+        </aside>
 
-      <footer className="footer">&copy; 2022 Wild Piccolo</footer>
-    </div>
+        <main className="main">
+          <Routes>
+            <Route path="/catalog" element={<Products />} /> 
+            <Route path="/" element={<Home />} /> 
+          </Routes>
+        </main>
+
+        <footer className="footer">&copy; 2022 Wild Piccolo</footer>
+      </div>
+    </Router>
   );
 }
 
