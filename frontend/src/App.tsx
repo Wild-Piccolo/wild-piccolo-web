91db3fe69ab1225dfb43ffde98ce1d1d5abeee0c
Albert Chin <albertchin.py@gmail.com>
2025-11-24 06:09:48 -0500

    Lab 6: connect Products page to API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,17 @@
-git status
+import React from "react";
+import Products from "./Products/Products";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Wild Piccolo</h1>
+      </header>
+      <main>
+        <Products />
+      </main>
+    </div>
+  );
+}
+
+export default App;
